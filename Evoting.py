abd5ee39a5c18f5638be95a69312046fed7de2a3
Avyaan Verma <114806843+avyaanverma@users.noreply.github.com>
2023-08-10 22:49:57 +0530

    Evoting in python completed for logic part

--- Evoting.py
+++ Evoting.py
@@ -1,36 +1,48 @@
-candidateSys = {"modi":0, "mamta": 0, "rahul": 0 }
-def setVote(candidateNo):
+import json
+# javascript object notation
+def main():
+    # Opening the json file 
+    file1 = open('election.json')
+    candidateSys = json.load(file1)
+    
+    # use json file
+    print("Candidates available in voting: ")
+    print("1.modi 2.mamta 3.rahul")
+
+    while(True):
+        # try:
+        no = int(input("press indivdual no to vote : "))
+        break
+        #  except TypeError:
+        #  continue
+    setVote(no, candidateSys)
+
+    print(f"1.modi : {candidateSys['candidates'][0]['votes']} 2.mamta: {candidateSys['candidates'][1]['votes']} 3.rahul: {candidateSys['candidates'][2]['votes']}")
+
+    # saving the json file in electionEnd
+    with open('electionEnd.json' , 'w') as file2:
+        data = json.dump(candidateSys, file2)
+def setVote(candidateNo , candidateSys):
     match candidateNo:
         case 1:
-            m = candidateSys.get("modi")
+            m = candidateSys['candidates'][0]['votes']
             m += 1
-            candidateSys["modi"] = m
+            candidateSys['candidates'][0]['votes'] = m 
+            
         case 2:
-            m = candidateSys.get("mamta")
+            m = candidateSys['candidates'][1]['votes']
             m += 1
-            candidateSys["mamta"] = m
+            candidateSys['candidates'][1]['votes'] = m 
         case 3:
-            m = candidateSys.get("rahul")
+            m = candidateSys['candidates'][2]['votes']
             m += 1
-            candidateSys["rahul"] = m
+            candidateSys['candidates'][2]['votes'] = m 
         case default:
             print(" Please choose correct symbols")
 
 
-# use json file
-print("Candidates available in voting: ")
-print("1.modi 2.mamta 3.rahul")
-
-while(True):
-    # try:
-    no = int(input("press indivdual to vote : "))
-    print(type(no))
-    break
-    # except TypeError:
-    #     continue
-setVote(no)
-
-print(f'1.modi : {candidateSys["modi"]} 2.mamta: {candidateSys["mamta"]}  3.rahul: {candidateSys["rahul"]}')
+if __name__ == "__main__":
+    main()
 
 
 
